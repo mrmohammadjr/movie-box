7e7add9889b70b84d501ddcf45194da6900ad393
mrmohammadjr <javadrma.2017@gmail.com>
2025-10-05 21:17:38 +0330

    jhj

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const prisma = global.prisma || new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
